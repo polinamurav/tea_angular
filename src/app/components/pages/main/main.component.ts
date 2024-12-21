1d72f9fc42da0234be73790e85825bdc3f4eaa67
Polina Muravieva <polinamur3@gmail.com>
2024-12-21 21:40:51 +0300

    сделана главная страница

--- src/app/components/pages/main/main.component.ts
+++ src/app/components/pages/main/main.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Observable, Subject, Subscription} from "rxjs";
+
+declare var $: any;
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent implements OnInit {
+export class MainComponent implements OnInit, OnDestroy {
+  private subscription: Subscription | null = null;
+  private observable: Observable<number>;
 
-  constructor() { }
+  isModalVisible: boolean = false;
+
+  constructor() {
+    this.observable = new Observable((observer) => {
+      setTimeout(() => {
+        observer.next()
+      }, 10000);
+    })
+  }
 
   ngOnInit(): void {
+    $('#accordion').accordion();
+
+    this.subscription = this.observable.subscribe({
+      next: value => { this.openModal() }
+    })
+  }
+
+  ngOnDestroy() {
+    this.subscription?.unsubscribe();
+  }
+
+  openModal() {
+    this.isModalVisible = true;
+  }
+
+  closeModal() {
+    this.isModalVisible = false;
   }
 
 }
