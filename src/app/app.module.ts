38add544a3528b6fa3e20ed1656566fcce61e4df
Polina Muravieva <polinamur3@gmail.com>
2024-12-21 20:49:13 +0300

    создание роутинга

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/pages/main/main.component';
+import { ProductComponent } from './components/pages/product/product.component';
+import { ProductsComponent } from './components/pages/products/products.component';
+import { OrderComponent } from './components/pages/order/order.component';
+import { HeaderComponent } from './components/common/header/header.component';
+import { FooterComponent } from './components/common/footer/footer.component';
+import {RouterLinkActive, RouterLinkWithHref, RouterOutlet} from "@angular/router";
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    ProductComponent,
+    ProductsComponent,
+    OrderComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterOutlet,
+    AppRoutingModule,
+    RouterLinkWithHref,
+    RouterLinkActive
   ],
   providers: [],
   bootstrap: [AppComponent]
